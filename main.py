c7188dcf3486383d835513b30abc171e9e476323
Bsbarmangio <shkrbabar@gmail.com>
2025-07-20 13:30:03 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import requests
 import telebot
 import os
+from telebot import apihelper
+
+apihelper.READ_TIMEOUT = 60
+apihelper.CONNECTION_TIMEOUT = 30
 
 BOT_TOKEN = "8165194741:AAEHi3W8ZYZIzHHPBXUN1Kkk0r4zK-mtIDk"
 OWNER_ID = 6896590701  # Your Telegram User ID
